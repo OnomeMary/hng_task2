397809653231c580dc9a8f2a420de65839a30cc1
Fumike <32973692+OnomeMary@users.noreply.github.com>
2018-04-11 01:25:11 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="clock_style.css">
+	
+	
+	<title>HNG4</title>
+</head>
+
+
+<body>
+	<h1>Hello World! We Just got Married. </h1>
+	
+<?php
+	
+	$date = date('<p>Y/m/d H:i:s</p>');
+	echo "$date";
+?>
+				
+</body>
+</html>
